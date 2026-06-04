60c7cfb35035e5407a09393bf933e8cbd0a092d5
Karol Cheverria <cheverriakarolvalentina@gmail.com>
2026-06-04 16:55:54 -0300

    ocultar google

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,12 @@
 import HomePage from "@/features/home/HomePage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  robots: {
+    index: false,
+    follow: true,
+  },
+};
 
 export default function Home() {
   return <HomePage />;
